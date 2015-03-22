199914a560c6a050b135138c0faee4fe9774a3a9
Ashwini Srivastava <asrivatava2012@my.fit.edu>
2015-03-22 14:29:11 -0400

    Ashwini Srivastava progrqamming Assignment 2 submittion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function is based on the example provided for mean
+## in the assignment.
+## Input x: is a square invertible matrix 
+##   	  (It is assumed it's always invertible)
+## Return : a list containing functions to
+##              -> set the matrix
+##              -> get the matrix
+##              -> set the inverse
+##              -> get the inverse
+##        List then used as the input to cacheSolve() function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## Function is based on the example provided for mean
+	  ## in the assignment.
+	  ## Input x: is a square invertible matrix 
+        ##   	  (It is assumed it's always invertible)
+        ## Return : a list containing functions to
+        ##              -> set the matrix
+        ##              -> get the matrix
+        ##              -> set the inverse
+        ##              -> get the inverse
+        ##        List then used as the input to cacheSolve() function
+        
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <- NULL
+        }
+        get <-function() x
+        setinv <- function(inverse) m <<- inverse 
+        getinv <- function() m
+        list(set=set, get = get, 
+		setinv = setinv, 
+		getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Function is based on the example provided for mean
+## in the assignment.
+## Input x: makeCacheMatrix() o/p
+## return : original matrix inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        ## Function is based on the example provided for mean
+	  ## in the assignment.
+        ## Input x: makeCacheMatrix() o/p
+        ## return : original matrix inverse
+        
+        m = x$getinv()
+        
+        # In case inverse exist and cached
+        if (!is.null(m)){
+                # As output get from the cache 
+                message("getting cached data")
+                return(m)
+        }
+        
+        # else, cal. the inverse 
+        data = x$get()
+        m = solve(data, ...)
+        
+        # And set the value
+        x$setinv(m)
+        
+        m
 }
